ea0ff90b6ec35fad85f20077e801838508789917
meeedbkr <meeedbk00@gmail.com>
2023-03-29 14:30:32 +0000

    Say Hello Done!
    create a script that prints 'hello, World! ' to the console while storing the string in a variable

---
+++ easy/01-Introduction/Say Hello, World! With Python.py
@@ -0,0 +1,5 @@
+# Create a string variable named "my_string" and assign the value "Hello, World!" to it.
+my_string = "Hello, World!"
+
+# Print the value of "my_string" to the console.
+print(my_string)
