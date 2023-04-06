e6773969d779d67a6ce1c66c7719075b13370004
meeedbkr <meeedbk00@gmail.com>
2023-04-06 21:39:35 +0000

    Integers Come In All Sizes

---
+++ easy/05-Math/integerSizes.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# take four integer inputs
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# calculate the result of a^b + c^d
+result = a**b + c**d
+
+# print the result
+print(result)
+
+
+
+
