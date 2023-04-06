2ec64175ad6092aa62d14ba80a6e3a3bddf6e435
meeedbkr <meeedbk00@gmail.com>
2023-04-06 21:38:14 +0000

    Mod Divmod

---
+++ easy/05-Math/divmod.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Get two integer inputs from the user
+a = int(input())
+b = int(input())
+
+# Print the integer division of a by b (i.e. a // b)
+print(a // b)
+
+# Print the remainder of a divided by b (i.e. a % b)
+print(a % b)
+
+# Print the result of divmod(a, b), which returns a tuple of (a // b, a % b)
+print(divmod(a, b))
+
+
