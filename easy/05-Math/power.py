c6abee433dda5d28376c9a0b3ac34140e6343698
meeedbkr <meeedbk00@gmail.com>
2023-04-06 21:38:52 +0000

    Power - Mod Power

---
+++ easy/05-Math/power.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Take three integer inputs
+a = int(input())
+b = int(input())
+m = int(input())
+
+# Print a to the power of b (i.e., a raised to the b-th power)
+print(pow(a,b))
+
+# Print the remainder when a to the power of b is divided by m
+print(pow(a,b,m))
+
+
+
